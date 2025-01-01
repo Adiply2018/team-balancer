d7eb37953c14656ee3e115c9b8b8c603e82908f8
Adiply2018 <adiplys@gmail.com>
2025-01-01 21:06:20 +0900

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import { Toaster } from "sonner";
 import TeamBalancer from "./features/team-builder";
 import { Root } from "@radix-ui/react-slot";
 import RootLayout from "./components/layouts/root";
-import { HttpRedirect } from "./components/http-provider";
+import { AlertCircle } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 function App() {
   return (
@@ -10,6 +12,23 @@ function App() {
       <RootLayout>
         <div className="container mx-auto p-4 space-y-4">
           <TeamBalancer />
+
+          {window.location.protocol === "https:" && (
+            <Alert>
+              <AlertCircle className="h-4 w-4" />
+              <AlertTitle>
+                大人の理由(怠惰)によりhttpsだと動かないので、httpでアクセスしてください。
+              </AlertTitle>
+              <AlertDescription>
+                <a
+                  href="http://momongapp.site"
+                  className="underline hover:text-blue-500"
+                >
+                  http://momongapp.site
+                </a>
+              </AlertDescription>
+            </Alert>
+          )}
           <Toaster position="top-right" />
         </div>
       </RootLayout>
